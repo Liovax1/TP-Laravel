01d6fc8fb497158a7769da8a82224b6a51e7ae9d
patrice <patrice.speziali@ac-bordeaux.fr>
2024-01-04 14:05:44 +0100

    routes/web.php - suppression des routes

--- routes/web.php
+++ routes/web.php
@@ -22,31 +22,3 @@ Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
 
-
-Route::get('/aPropos',
-     'App\Http\Controllers\PagesController@aPropos')->name('aPropos') ;
-
-Route::get('/balises',
-     'App\Http\Controllers\PagesBaliseController@balises')->name('balises') ;
-
-Route::get('/balise/{id}',
-     'App\Http\Controllers\PagesBaliseController@balise')->name('balise') ;
-
-Route::get('/formAddBalise',
-     'App\Http\Controllers\PagesBaliseController@create')->name('formAddBalise') ;
-
-Route::post('/formAddBalise',
-     'App\Http\Controllers\PagesBaliseController@store')->name('formAddBalise') ;
-
-Route::get('/formEditBalise/{id}',
-     'App\Http\Controllers\PagesBaliseController@edit')->name('formEditBalise') ;
-
-Route::post('/formEditBalise',
-     'App\Http\Controllers\PagesBaliseController@upDate')->name('formEditBalise') ;
-
-Route::get('/deleteBalise/{id}',
-     'App\Http\Controllers\PagesBaliseController@delete')->name('deleteBalise') ;
-
-
-Route::get('/releveBalise/{id}',
-'App\Http\Controllers\PagesController@releveBalise')->name('releveBalise') ;
