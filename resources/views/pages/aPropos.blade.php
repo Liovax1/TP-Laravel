8de01af00b9539778df8085a611255321921811c
patrice <patrice.speziali@ac-bordeaux.fr>
2024-01-04 14:11:24 +0100

    Suppression des pages aPropos.blade.php, ...

--- resources/views/pages/aPropos.blade.php
+++
@@ -1,5 +0,0 @@
-<h1>A Propos de météofox</h1>
-@auth <p>utilisateur authentifié</p>@endauth
-@guest <p>utilisateur non authentifié<P>@endguest
-
-<p>url de la page 'aPropos' : <a href={{route('aPropos');}}> {{route('aPropos');}} </a></p>
